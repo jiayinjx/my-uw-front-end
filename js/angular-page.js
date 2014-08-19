8002f14fd7d710ce62b9453e7f758e3f5fe96d33
Tim Levett <levett@wisc.edu>
2014-08-19 08:00:34 -0500

    Made it so notifications are stateful

--- js/angular-page.js
+++ js/angular-page.js
@@ -34,21 +34,22 @@
 
   app.controller('NotificationController', function(){
     this.notifications = notes;
-    this.isRead = function(index){
-      return notification[index].read;
-    };
 
     this.getClass = function getClass(index, notifications) {
       return {
-        read : notification[index].read
+        unread : notifications[index].unread
       };
+    }
+
+    this.markRead = function markRead(index, notifications) {
+      notifications[index].unread = false;
     }
   });
 
 
 
   var notes = [
-    {notificationId: 1, text: "test text", read : true},
-    {notificationId: 2, text: "test text 2", read : false},
+    {notificationId: 1, text: "test text", unread : true},
+    {notificationId: 2, text: "test text 2", unread : false},
   ];
 })();
